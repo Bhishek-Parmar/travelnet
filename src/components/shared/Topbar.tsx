d3f6e67c8c89d961a570b1ba52c1fce20df76171
Bhishek-Parmar <itsbhishek@gmail.com>
2023-10-30 18:29:30 +0530

    more changes

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -18,12 +18,10 @@ const Topbar = () => {
     <section className="topbar">
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
-          <img
-            src="/assets/images/logo.svg"
-            alt="logo"
-            width={130}
-            height={325}
-          />
+          <h1 className="text-4xl font-bold">
+            Travel
+            <span className="text-indigo-400">Net</span>
+          </h1>
         </Link>
 
         <div className="flex gap-4">
